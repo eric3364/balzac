ed68f18cad6d1f390cead270e3af67efb4c54eff
eric3364 <eric.combalbert@next-u.fr>
2025-08-17 09:45:56 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Toaster } from "sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -22,8 +21,7 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <AuthProvider>
       <TooltipProvider>
-        <Toaster />
-        <Sonner />
+        <Toaster /> {/* Notifications Sonner */}
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Index />} />
@@ -36,7 +34,6 @@ const App = () => (
             <Route path="/session-progress" element={<SessionProgress />} />
             <Route path="/admin" element={<Admin />} />
             <Route path="/admin-auth" element={<AdminAuth />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
